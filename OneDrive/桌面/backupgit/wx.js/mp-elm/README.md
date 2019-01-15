7e5c09123fcb8beed13e578e4731e2ae7bec928a
miles97 <xiaokeaicg@hotmail.com>
2019-01-15 10:37:30 +0800

    MVVM from weixin.miniprogram.js

---
+++ OneDrive/桌面/backupgit/wx.js/mp-elm/README.md
@@ -0,0 +1,6 @@
+# 微信小程序重构基于vue的cangdu的elm项目
+
+部分代码参考来自cangdu的项目 [elm](http://elm.cangdu.org/)
+
+
+
---
+++ OneDrive/桌面/backupgit/wx.js/mp-elm/pages/nameList/nameList.js
@@ -0,0 +1,26 @@
+// pages/nameList/nameList.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    nameList:{
+      locationName:"佳都大厦",
+      
+    },
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
