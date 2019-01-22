954453f40f7d01fac9dc96c7cb0c24b2a5b30340
miles97 <xiaokeaicg@hotmail.com>
2019-01-22 11:29:50 +0800

    add page itemDetail

--- OneDrive/桌面/backupgit/wx.js/mp-elm/pages/itemDetail/itemDetail.js
+++ OneDrive/桌面/backupgit/wx.js/mp-elm/pages/itemDetail/itemDetail.js
@@ -12,14 +12,46 @@ Component({
    */
   data: {
     showMenu: false,
-    itemChoose: false,
+    itemChoose: true,
     showCartList: false,
+    showScore: false,
+    shopDetailData:{
+      "rating":5,
+      "damn":40,
+
+    },
+    ratingScoresData:{
+      "service_score":3,
+      "food_score":3,
+      "order_lead_time":20,
+    },
+    itemDetail:{
+      "shopname":"张亮麻辣烫",
+      "intro":"张亮麻辣烫，我们不一样!",
+      "exintro":"水煮，干拌，轻食麻辣烫，任你选择。"
+    }
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    changeshowitem:function(){
+      var itemChoose = this.data.itemChoose;
+      // this.data.itemChoose=!this.data.itemChoose;
+      this.setData({
+        itemChoose: true, 
+        showScore: false,
+      })
+    },
+    changeshowscore: function () {
+      var showScore = this.data.showScore;
+      // this.data.showScore = !this.data.showScore;
+      this.setData({
+        showScore: true,
+        itemChoose: false,
+      })
+    },
 
   }
 })
